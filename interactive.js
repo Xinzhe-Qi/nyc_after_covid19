d47602d0771345967c4e10994d20299dffafa421
Xinzhe <xinzhe@XZs-MacBook-Pro.local>
2021-04-12 00:42:53 -0400

    interactive part

--- interactive.js
+++ interactive.js
@@ -1,14 +1,51 @@
-// !preview r2d3 data=c(0.3, 0.6, 0.8, 0.95, 0.40, 0.20)
-//
-// r2d3: https://rstudio.github.io/r2d3
-//
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 90, left: 40},
+    width = 1000 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
-var barHeight = Math.ceil(height / data.length);
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-svg.selectAll('rect')
-  .data(data)
-  .enter().append('rect')
-    .attr('width', function(d) { return d * width; })
-    .attr('height', barHeight)
-    .attr('y', function(d, i) { return i * barHeight; })
-    .attr('fill', 'steelblue');
+d3.csv("busiCount.csv", function(data) {
+
+  // sort data
+  data.sort(function(b, a) {
+    return a.count - b.count;
+  });
+
+  // X axis
+  var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(data.map(function(d) { return d.industry; }))
+    .padding(0.2);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 13000])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Bars
+  svg.selectAll("mybar")
+    .data(data)
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return x(d.industry); })
+      .attr("y", function(d) { return y(d.count); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.count); })
+      .attr("fill", "#69b3a2");
+});
